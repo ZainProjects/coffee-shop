019cf2bcfbcfad99cb5f5a2aa491f54f709edc92
unknown <zainprojects73@gmail.com>
2025-07-26 16:37:36 +0530

    Initial coffee shop website

---
+++ index.js
@@ -0,0 +1,9 @@
+// Optional: Smooth scroll for in-page links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
