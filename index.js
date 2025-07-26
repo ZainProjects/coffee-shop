cdc07a15632f521cc1fc29adb9800ab7c4fd1fe0
unknown <zainprojects73@gmail.com>
2025-07-26 17:12:55 +0530

    integrated tawk.to

--- index.js
+++ index.js
@@ -7,16 +7,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-
-// Tawk.to Integration via JS file
-(function() {
-    var Tawk_API = Tawk_API || {};
-    var Tawk_LoadStart = new Date();
-    var s1 = document.createElement("script");
-    var s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/63a123456789abcdef/DEFAULT';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-})();
